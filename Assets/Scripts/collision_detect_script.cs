dab30a8d567905fa270563b884a3fa74fcb64862
Bob The Builder <bob@the.builder>
2021-04-15 23:31:59 +0530

    New commit

---
+++ Assets/Scripts/collision_detect_script.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collision_detect_script : MonoBehaviour
+{
+    float horizontalInput;
+    float verticalInput;
+    float speed = 5;
+    int coin = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
+        transform.Translate(Vector3.forward * verticalInput * speed * Time.deltaTime);
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<Renderer>().material.color == Color.red)
+        {
+            coin = coin + 1;
+            print("Coins: " + coin);
+        }
+        else
+        {
+            coin = coin - 1;
+            print("Coins: " + coin);
+        }
+    }
+}
